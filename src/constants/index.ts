36ea38a5879c4da76f96e8c3c892ee3045fbe23d
Vladimir Agaev <agaev.vladimir@gmail.com>
2023-10-16 11:26:36 +0100

    feat: add .env

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+const { VITE_GRID_X_MAX, VITE_GRID_Y_MAX } = import.meta.env;
+
+export { VITE_GRID_X_MAX, VITE_GRID_Y_MAX };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GRID_X_MAX: string;
+  readonly VITE_GRID_Y_MAX: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
